ee209183e1746ee3b59573c2b36b9cb1cd5c602b
Pchiroli <pablochiroli@hotmail.com>
2024-04-15 21:56:16 -0300

    final entrega

--- preentrega/src/App.jsx
+++ preentrega/src/App.jsx
@@ -1,12 +1,13 @@
 //import { useState } from 'react'
 import './App.css'
+import ItemListContainer from './assets/components/ItemListContainer'
 import NavBar from './assets/components/NavBar'
 
 function App() {
     return (
     <>
     <NavBar/>
-    
+     <ItemListContainer texto={"bienvenidos"} />
     </>
   )
 }
